6afe9a626d61c672fe1e26501a89eb94bc68ecb8
Jason Reilly <jdr0887@gmail.com>
2016-03-29 09:41:28 -0400

    initial commit

---
+++ ncnexus-clean-messaging/src/main/java/edu/unc/mapseq/messaging/ncnexus/casava/NCNEXUSCleanMessageListener.java
@@ -0,0 +1,108 @@
+package edu.unc.mapseq.messaging.ncnexus.casava;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.WorkflowDAO;
+import edu.unc.mapseq.dao.WorkflowRunAttemptDAO;
+import edu.unc.mapseq.dao.WorkflowRunDAO;
+import edu.unc.mapseq.dao.model.Workflow;
+import edu.unc.mapseq.dao.model.WorkflowRun;
+import edu.unc.mapseq.dao.model.WorkflowRunAttempt;
+import edu.unc.mapseq.dao.model.WorkflowRunAttemptStatusType;
+import edu.unc.mapseq.workflow.WorkflowException;
+import edu.unc.mapseq.workflow.impl.AbstractSampleMessageListener;
+import edu.unc.mapseq.workflow.model.WorkflowMessage;
+
+public class NCNEXUSCleanMessageListener extends AbstractSampleMessageListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(NCNEXUSCleanMessageListener.class);
+
+    public NCNEXUSCleanMessageListener() {
+        super();
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        logger.debug("ENTERING onMessage(Message)");
+
+        String messageValue = null;
+
+        try {
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                messageValue = textMessage.getText();
+                logger.debug("Received TextMessage: {}", messageValue);
+            }
+        } catch (JMSException e2) {
+            e2.printStackTrace();
+        }
+
+        if (StringUtils.isEmpty(messageValue)) {
+            logger.warn("message value is empty");
+            return;
+        }
+
+        logger.info("messageValue: {}", messageValue);
+
+        ObjectMapper mapper = new ObjectMapper();
+        WorkflowMessage workflowMessage = null;
+
+        try {
+            workflowMessage = mapper.readValue(messageValue, WorkflowMessage.class);
+            if (workflowMessage.getEntities() == null) {
+                logger.error("json lacks entities");
+                return;
+            }
+        } catch (IOException e) {
+            logger.error("BAD JSON format", e);
+            return;
+        }
+
+        MaPSeqDAOBeanService daoBean = getWorkflowBeanService().getMaPSeqDAOBeanService();
+        WorkflowDAO workflowDAO = daoBean.getWorkflowDAO();
+        WorkflowRunDAO workflowRunDAO = daoBean.getWorkflowRunDAO();
+        WorkflowRunAttemptDAO workflowRunAttemptDAO = daoBean.getWorkflowRunAttemptDAO();
+
+        Workflow workflow = null;
+        try {
+            List<Workflow> workflowList = workflowDAO.findByName("NCNEXUSClean");
+            if (workflowList == null || (workflowList != null && workflowList.isEmpty())) {
+                logger.error("No Workflow Found: {}", "NCNEXUSClean");
+                return;
+            }
+            workflow = workflowList.get(0);
+        } catch (MaPSeqDAOException e) {
+            logger.error("ERROR", e);
+        }
+
+        try {
+            WorkflowRun workflowRun = createWorkflowRun(workflowMessage, workflow);
+
+            Long workflowRunId = workflowRunDAO.save(workflowRun);
+            workflowRun.setId(workflowRunId);
+
+            WorkflowRunAttempt attempt = new WorkflowRunAttempt();
+            attempt.setStatus(WorkflowRunAttemptStatusType.PENDING);
+            attempt.setWorkflowRun(workflowRun);
+            workflowRunAttemptDAO.save(attempt);
+
+        } catch (WorkflowException | MaPSeqDAOException e1) {
+            logger.error(e1.getMessage(), e1);
+        }
+
+    }
+
+}
---
+++ ncnexus-clean-workflow/src/main/java/edu/unc/mapseq/workflow/ncnexus/casava/NCNEXUSCleanWorkflow.java
@@ -0,0 +1,162 @@
+package edu.unc.mapseq.workflow.ncnexus.casava;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.renci.jlrm.condor.CondorJob;
+import org.renci.jlrm.condor.CondorJobBuilder;
+import org.renci.jlrm.condor.CondorJobEdge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.unc.mapseq.dao.model.Flowcell;
+import edu.unc.mapseq.dao.model.Sample;
+import edu.unc.mapseq.dao.model.WorkflowRunAttempt;
+import edu.unc.mapseq.module.core.RemoveCLI;
+import edu.unc.mapseq.workflow.WorkflowException;
+import edu.unc.mapseq.workflow.impl.AbstractSampleWorkflow;
+import edu.unc.mapseq.workflow.impl.SampleWorkflowUtil;
+import edu.unc.mapseq.workflow.impl.WorkflowJobFactory;
+
+public class NCNEXUSCleanWorkflow extends AbstractSampleWorkflow {
+
+    private static final Logger logger = LoggerFactory.getLogger(NCNEXUSCleanWorkflow.class);
+
+    public NCNEXUSCleanWorkflow() {
+        super();
+    }
+
+    @Override
+    public String getName() {
+        return NCNEXUSCleanWorkflow.class.getSimpleName().replace("Workflow", "");
+    }
+
+    @Override
+    public String getVersion() {
+        ResourceBundle bundle = ResourceBundle.getBundle("edu/unc/mapseq/workflow/ncnexus/baseline/workflow");
+        String version = bundle.getString("version");
+        return StringUtils.isNotEmpty(version) ? version : "0.0.1-SNAPSHOT";
+    }
+
+    @Override
+    public Graph<CondorJob, CondorJobEdge> createGraph() throws WorkflowException {
+        logger.info("ENTERING createGraph()");
+
+        DirectedGraph<CondorJob, CondorJobEdge> graph = new DefaultDirectedGraph<CondorJob, CondorJobEdge>(
+                CondorJobEdge.class);
+
+        int count = 0;
+
+        Set<Sample> sampleSet = getAggregatedSamples();
+        logger.info("sampleSet.size(): {}", sampleSet.size());
+        WorkflowRunAttempt attempt = getWorkflowRunAttempt();
+
+        String siteName = getWorkflowBeanService().getAttributes().get("siteName");
+        String flowcellStagingDirectory = getWorkflowBeanService().getAttributes().get("flowcellStagingDirectory");
+
+        Set<Flowcell> flowcells = new HashSet<Flowcell>();
+        for (Sample sample : sampleSet) {
+            if (!flowcells.contains(sample.getFlowcell())) {
+                flowcells.add(sample.getFlowcell());
+            }
+        }
+        Collections.synchronizedSet(flowcells);
+
+        for (Flowcell flowcell : flowcells) {
+            File flowcellStagingDir = new File(flowcellStagingDirectory, flowcell.getName());
+            if (flowcellStagingDir.exists()) {
+                CondorJobBuilder builder = WorkflowJobFactory.createJob(++count, RemoveCLI.class, attempt.getId())
+                        .siteName(siteName);
+                builder.addArgument(RemoveCLI.FILE, flowcellStagingDir.getAbsolutePath());
+            }
+        }
+
+        for (Sample sample : sampleSet) {
+
+            if ("Undetermined".equals(sample.getBarcode())) {
+                continue;
+            }
+
+            logger.debug(sample.toString());
+
+            File outputDirectory = new File(sample.getOutputDirectory(), "NCNEXUSBaseline");
+
+            List<File> readPairList = SampleWorkflowUtil.getReadPairList(sample);
+            logger.debug("readPairList.size(): {}", readPairList.size());
+
+            if (readPairList.size() != 2) {
+                throw new WorkflowException("readPairList != 2");
+            }
+
+            File r1FastqFile = readPairList.get(0);
+            String r1FastqRootName = SampleWorkflowUtil.getRootFastqName(r1FastqFile.getName());
+
+            File r2FastqFile = readPairList.get(1);
+            String r2FastqRootName = SampleWorkflowUtil.getRootFastqName(r2FastqFile.getName());
+
+            String fastqLaneRootName = StringUtils.removeEnd(r2FastqRootName, "_R2");
+
+            try {
+
+                File saiR1OutFile = new File(outputDirectory, r1FastqRootName + ".sai");
+
+                File saiR2OutFile = new File(outputDirectory, r2FastqRootName + ".sai");
+
+                File bwaSAMPairedEndOutFile = new File(outputDirectory, fastqLaneRootName + ".sam");
+
+                File fixRGOutput = new File(outputDirectory,
+                        bwaSAMPairedEndOutFile.getName().replace(".sam", ".rg.bam"));
+
+                File picardAddOrReplaceReadGroupsIndexOut = new File(outputDirectory,
+                        fixRGOutput.getName().replace(".bam", ".bai"));
+
+                File picardMarkDuplicatesMetricsFile = new File(outputDirectory,
+                        fixRGOutput.getName().replace(".bam", ".md.metrics"));
+
+                File picardMarkDuplicatesOutput = new File(outputDirectory,
+                        fixRGOutput.getName().replace(".bam", ".md.bam"));
+
+                File picardMarkDuplicatesIndexOut = new File(outputDirectory,
+                        picardMarkDuplicatesOutput.getName().replace(".bam", ".bai"));
+
+                File realignTargetCreatorOut = new File(outputDirectory,
+                        picardMarkDuplicatesOutput.getName().replace(".bam", ".targets.intervals"));
+
+                File indelRealignerOut = new File(outputDirectory,
+                        picardMarkDuplicatesOutput.getName().replace(".bam", ".ir.bam"));
+
+                File picardFixMateOutput = new File(outputDirectory,
+                        indelRealignerOut.getName().replace(".bam", ".fm.bam"));
+
+                File picardFixMateIndexOut = new File(outputDirectory,
+                        picardFixMateOutput.getName().replace(".bam", ".bai"));
+
+                // new job
+                CondorJobBuilder builder = WorkflowJobFactory
+                        .createJob(++count, RemoveCLI.class, attempt.getId(), sample.getId()).siteName(siteName);
+                builder.addArgument(RemoveCLI.FILE, saiR1OutFile.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, saiR2OutFile.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, bwaSAMPairedEndOutFile.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, picardMarkDuplicatesOutput.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, picardMarkDuplicatesIndexOut.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, indelRealignerOut.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, picardFixMateOutput.getAbsolutePath())
+                        .addArgument(RemoveCLI.FILE, picardFixMateIndexOut.getAbsolutePath());
+
+            } catch (Exception e) {
+                throw new WorkflowException(e);
+            }
+        }
+
+        return graph;
+    }
+
+}
