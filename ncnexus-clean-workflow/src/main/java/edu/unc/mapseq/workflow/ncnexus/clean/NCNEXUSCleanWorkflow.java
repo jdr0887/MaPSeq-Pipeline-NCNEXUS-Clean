1f2fa9f3119c8692a3ca5934d153b1d1b049c8fc
Jason Reilly <jdr0887@gmail.com>
2016-04-22 14:20:51 -0400

    adding removal of unaligned directories

--- ncnexus-clean-workflow/src/main/java/edu/unc/mapseq/workflow/ncnexus/clean/NCNEXUSCleanWorkflow.java
+++ ncnexus-clean-workflow/src/main/java/edu/unc/mapseq/workflow/ncnexus/clean/NCNEXUSCleanWorkflow.java
@@ -78,6 +78,24 @@ public class NCNEXUSCleanWorkflow extends AbstractSequencingWorkflow {
                         .siteName(siteName);
                 builder.addArgument(RemoveCLI.FILE, flowcellStagingDir.getAbsolutePath());
             }
+
+            Set<Integer> laneSet = new HashSet<>();
+            sampleSet.forEach(a -> laneSet.add(a.getLaneIndex()));
+            Collections.synchronizedSet(laneSet);
+
+            File bclDir = new File(flowcell.getBaseDirectory());
+            File bclFlowcellDir = new File(bclDir, flowcell.getName());
+
+            for (Integer lane : laneSet) {
+                File unalignedDir = new File(bclFlowcellDir, String.format("%s.%d", "Unaligned", lane.toString()));
+                CondorJobBuilder builder = WorkflowJobFactory.createJob(++count, RemoveCLI.class, attempt.getId())
+                        .siteName(siteName);
+                builder.addArgument(RemoveCLI.FILE, unalignedDir);
+                CondorJob removeUnalignedDirectoryJob = builder.build();
+                logger.info(removeUnalignedDirectoryJob.toString());
+                graph.addVertex(removeUnalignedDirectoryJob);
+            }
+
         }
 
         for (Sample sample : sampleSet) {
