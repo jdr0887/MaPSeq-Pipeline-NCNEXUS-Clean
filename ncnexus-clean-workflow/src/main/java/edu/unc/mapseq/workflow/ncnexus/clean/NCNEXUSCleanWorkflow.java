9f40f5a837330aeb3200cc7bc3855184ae35cc35
Jason Reilly <jdr0887@gmail.com>
2017-01-03 09:24:31 -0500

    removing pipeline files from clean...added remove job to baseline

--- ncnexus-clean-workflow/src/main/java/edu/unc/mapseq/workflow/ncnexus/clean/NCNEXUSCleanWorkflow.java
+++ ncnexus-clean-workflow/src/main/java/edu/unc/mapseq/workflow/ncnexus/clean/NCNEXUSCleanWorkflow.java
@@ -3,11 +3,8 @@ package edu.unc.mapseq.workflow.ncnexus.clean;
 import java.io.File;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultDirectedGraph;
@@ -17,17 +14,13 @@ import org.renci.jlrm.condor.CondorJobEdge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
-import edu.unc.mapseq.dao.MaPSeqDAOException;
 import edu.unc.mapseq.dao.model.Flowcell;
 import edu.unc.mapseq.dao.model.Sample;
-import edu.unc.mapseq.dao.model.Workflow;
 import edu.unc.mapseq.dao.model.WorkflowRunAttempt;
 import edu.unc.mapseq.module.core.RemoveCLI;
 import edu.unc.mapseq.workflow.WorkflowException;
 import edu.unc.mapseq.workflow.core.WorkflowJobFactory;
 import edu.unc.mapseq.workflow.sequencing.AbstractSequencingWorkflow;
-import edu.unc.mapseq.workflow.sequencing.SequencingWorkflowJobFactory;
 import edu.unc.mapseq.workflow.sequencing.SequencingWorkflowUtil;
 
 public class NCNEXUSCleanWorkflow extends AbstractSequencingWorkflow {
@@ -77,93 +70,16 @@ public class NCNEXUSCleanWorkflow extends AbstractSequencingWorkflow {
             File bclFlowcellDir = new File(bclDir, flowcell.getName());
 
             for (Integer lane : laneSet) {
-                File unalignedDir = new File(bclFlowcellDir, String.format("%s.%d", "Unaligned", lane.toString()));
-                CondorJobBuilder builder = WorkflowJobFactory.createJob(++count, RemoveCLI.class, attempt.getId()).siteName(siteName);
-                builder.addArgument(RemoveCLI.FILE, unalignedDir);
-                CondorJob removeUnalignedDirectoryJob = builder.build();
-                logger.info(removeUnalignedDirectoryJob.toString());
-                graph.addVertex(removeUnalignedDirectoryJob);
+                File unalignedDir = new File(bclFlowcellDir, String.format("%s.%d", "Unaligned", lane));
+                if (unalignedDir.exists()) {
+                    CondorJobBuilder builder = WorkflowJobFactory.createJob(++count, RemoveCLI.class, attempt.getId()).siteName(siteName);
+                    builder.addArgument(RemoveCLI.FILE, unalignedDir);
+                    CondorJob removeUnalignedDirectoryJob = builder.build();
+                    logger.info(removeUnalignedDirectoryJob.toString());
+                    graph.addVertex(removeUnalignedDirectoryJob);
+                }
             }
 
-        }
-
-        MaPSeqDAOBeanService daoBean = getWorkflowBeanService().getMaPSeqDAOBeanService();
-
-        Workflow baselineWorkflow = null;
-        try {
-            List<Workflow> workflowList = daoBean.getWorkflowDAO().findByName("NCNEXUSBaseline");
-            if (CollectionUtils.isNotEmpty(workflowList)) {
-                baselineWorkflow = workflowList.get(0);
-            }
-        } catch (MaPSeqDAOException e1) {
-            e1.printStackTrace();
-        }
-
-        for (Sample sample : sampleSet) {
-
-            if ("Undetermined".equals(sample.getBarcode())) {
-                continue;
-            }
-
-            logger.debug(sample.toString());
-
-            List<File> readPairList = SequencingWorkflowUtil.getReadPairList(sample);
-            logger.debug("readPairList.size(): {}", readPairList.size());
-
-            if (readPairList.size() != 2) {
-                throw new WorkflowException("readPairList != 2");
-            }
-
-            File r1FastqFile = readPairList.get(0);
-            String r1FastqRootName = SequencingWorkflowUtil.getRootFastqName(r1FastqFile.getName());
-
-            File r2FastqFile = readPairList.get(1);
-            String r2FastqRootName = SequencingWorkflowUtil.getRootFastqName(r2FastqFile.getName());
-
-            String fastqLaneRootName = StringUtils.removeEnd(r2FastqRootName, "_R2");
-
-            try {
-
-                File outputDirectory = SequencingWorkflowUtil.createOutputDirectory(sample, baselineWorkflow);
-
-                File saiR1OutFile = new File(outputDirectory, r1FastqRootName + ".sai");
-
-                File saiR2OutFile = new File(outputDirectory, r2FastqRootName + ".sai");
-
-                File bwaSAMPairedEndOutFile = new File(outputDirectory, fastqLaneRootName + ".sam");
-
-                File fixRGOutput = new File(outputDirectory, bwaSAMPairedEndOutFile.getName().replace(".sam", ".rg.bam"));
-
-                File picardAddOrReplaceReadGroupsIndexOut = new File(outputDirectory, fixRGOutput.getName().replace(".bam", ".bai"));
-
-                File picardMarkDuplicatesMetricsFile = new File(outputDirectory, fixRGOutput.getName().replace(".bam", ".md.metrics"));
-
-                File picardMarkDuplicatesOutput = new File(outputDirectory, fixRGOutput.getName().replace(".bam", ".md.bam"));
-
-                File picardMarkDuplicatesIndexOut = new File(outputDirectory, picardMarkDuplicatesOutput.getName().replace(".bam", ".bai"));
-
-                File realignTargetCreatorOut = new File(outputDirectory, picardMarkDuplicatesOutput.getName().replace(".bam", ".targets.intervals"));
-
-                File indelRealignerOut = new File(outputDirectory, picardMarkDuplicatesOutput.getName().replace(".bam", ".ir.bam"));
-
-                File picardFixMateOutput = new File(outputDirectory, indelRealignerOut.getName().replace(".bam", ".fm.bam"));
-
-                File picardFixMateIndexOut = new File(outputDirectory, picardFixMateOutput.getName().replace(".bam", ".bai"));
-
-                // new job
-                CondorJobBuilder builder = SequencingWorkflowJobFactory.createJob(++count, RemoveCLI.class, attempt.getId(), sample.getId())
-                        .siteName(siteName);
-                builder.addArgument(RemoveCLI.FILE, saiR1OutFile.getAbsolutePath()).addArgument(RemoveCLI.FILE, saiR2OutFile.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, bwaSAMPairedEndOutFile.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, picardMarkDuplicatesOutput.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, picardMarkDuplicatesIndexOut.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, indelRealignerOut.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, picardFixMateOutput.getAbsolutePath())
-                        .addArgument(RemoveCLI.FILE, picardFixMateIndexOut.getAbsolutePath());
-
-            } catch (Exception e) {
-                throw new WorkflowException(e);
-            }
         }
 
         return graph;
